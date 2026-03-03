9e20996259b0384415b6531de050bc8fe25d4ce6
SolyanaZewdie <129341297+SolyanaZewdie@users.noreply.github.com>
2026-03-03 15:23:01 +0300

    Update Robot Program.py

--- Robot Program.py
+++ Robot Program.py
@@ -1,22 +1,38 @@
 t = int(input())
 for _ in range(t):
     n, x, k = map(int, input().split())
-    s = input()
-    if x == 0:
-        print(k // n)
-        continue
-    pos = x
-    ans = 0
-    for i in range(min(n, k)):
+    s = input().strip()
+    
+    pref = [0] * n
+    cur = 0
+    for i in range(n):
         if s[i] == 'L':
-            pos -= 1
+            cur -= 1
         else:
-            pos += 1
-        if pos == 0:
-            ans += 1
+            cur += 1
+        pref[i] = cur
+    
+    first_hit = -1
+    for i in range(n):
+        if x + pref[i] == 0:
+            first_hit = i + 1
             break
-    if ans == 0:
+    
+    if first_hit == -1 or first_hit > k:
         print(0)
         continue
-    ans += (k - i - 1) // n
-    print(ans)
+    
+    ans = 1
+    remaining = k - first_hit
+    
+    cycle_hit = -1
+    for i in range(n):
+        if pref[i] == 0:
+            cycle_hit = i + 1
+            break
+    
+    if cycle_hit == -1:
+        print(ans)
+    else:
+        ans += remaining // cycle_hit
+        print(ans)
