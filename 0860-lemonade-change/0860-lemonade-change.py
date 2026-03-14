ec90af40bcf88859375437841461bcd4882354dc
SolyanaZewdie <129341297+SolyanaZewdie@users.noreply.github.com>
2026-03-14 21:20:14 +0300

    Time: 3 ms (77.98%), Space: 15.2 MB (61.63%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        five=0
+        ten=0
+        
+        for b in bills:
+            if b==5:
+                five+=1
+            elif b==10:
+                if five==0:
+                    return False
+                five-=1
+                ten+=1
+            else:
+                if ten>0 and five>0:
+                    ten-=1
+                    five-=1
+                elif five>=3:
+                    five-=3
+                else:
+                    return False
+        return True
