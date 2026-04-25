fbc9563c6a64e4ac6d8995fc03321f0f36a6feb9
SolyanaZewdie <129341297+SolyanaZewdie@users.noreply.github.com>
2026-04-25 11:15:41 +0300

    Time: 0 ms (100%), Space: 12.7 MB (49.78%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,26 @@
+from collections import deque
+class Solution(object):
+    def isBipartite(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: bool
+        """
+        n = len(graph)
+        color = [-1] * n  
+        
+        for i in range(n):
+            if color[i] == -1:
+                queue = deque([i])
+                color[i] = 0
+                
+                while queue:
+                    node = queue.popleft()
+                    
+                    for neighbor in graph[node]:
+                        if color[neighbor] == -1:
+                            color[neighbor] = 1 - color[node]
+                            queue.append(neighbor)
+                        elif color[neighbor] == color[node]:
+                            return False
+                            
+        return True
